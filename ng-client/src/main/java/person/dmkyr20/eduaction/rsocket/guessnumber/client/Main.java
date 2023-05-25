c59b110cbfd90917caa9ab75f2a3fb570712812a
Dmytro Kyrychkov <dmkyr20@gmail.com>
2023-05-25 09:50:39 +0200

    Configured end of the game

--- ng-client/src/main/java/person/dmkyr20/eduaction/rsocket/guessnumber/client/Main.java
+++ ng-client/src/main/java/person/dmkyr20/eduaction/rsocket/guessnumber/client/Main.java
@@ -8,6 +8,7 @@ import reactor.core.publisher.Flux;
 import java.time.Duration;
 import java.util.Scanner;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
@@ -28,24 +29,56 @@ public class Main {
                 throw new RuntimeException("Connection isn't valid");
             }
 
-
             Flux<GuessNumberRequest> requestFlux = Flux.<Integer>create(
                     s -> {
                         while (!s.isCancelled()) {
                             System.out.println("Your number?");
-                            s.next(scanner.nextInt());
+                            String next = scanner.next();
+                            if("exit".equals(next)) {
+                                System.out.println("Bye!!!");
+                                s.complete();
+                                return;
+                            } else if (isInteger(next)) {
+                                s.next(Integer.parseInt(next));
+                            } else {
+                                System.out.println("Oops! The value is not an integer - " + next);
+                            }
                         }
                         s.complete();
                     })
                     .map(GuessNumberRequest::new);
 
+            AtomicInteger score = new AtomicInteger(0);
             client.chanel(requestFlux)
                     .map(GuessNumberResponse::getMessage)
                     .doOnNext(System.out::println)
+                    .doOnNext(n -> {
+                        if(n != null && n.startsWith("YES")) {
+                            System.out.println("You won, your score is: " + score.get());
+                            System.exit(0);
+                        }
+                    })
+                    .doOnNext(n -> score.incrementAndGet())
                     .subscribe();
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static boolean isInteger(String s) {
+        return isInteger(s,10);
+    }
+
+    public static boolean isInteger(String s, int radix) {
+        if(s.isEmpty()) return false;
+        for(int i = 0; i < s.length(); i++) {
+            if(i == 0 && s.charAt(i) == '-') {
+                if(s.length() == 1) return false;
+                else continue;
+            }
+            if(Character.digit(s.charAt(i),radix) < 0) return false;
+        }
+        return true;
     }
 }
